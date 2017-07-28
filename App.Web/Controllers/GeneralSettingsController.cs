5d2371e75963be6d3647c2cb490b2f6b5ade5158
Md. Ariful Islam <arifjmamun24@gmail.com>
2017-07-28 23:20:21 +0600

    Database backup module created.

--- App.Web/Controllers/GeneralSettingsController.cs
+++ App.Web/Controllers/GeneralSettingsController.cs
@@ -8,7 +8,6 @@ using System.Web.Mvc;
 using App.Data.Context;
 using App.Entity.Models;
 using EntityFramework.Extensions;
-using EntityFramework.Utilities;
 using WebGrease.Css.Extensions;
 
 namespace App.Web.Controllers
@@ -18,7 +17,7 @@ namespace App.Web.Controllers
         #region Private Fields
 
         private readonly CrmDbContext _db = new CrmDbContext();
-        private readonly List<string> _allowedLogoFileTypes = new List<string> { ".png", "jpg", ".jpeg", ".gif", ".bmp" };
+        private readonly List<string> _allowedLogoFileTypes = new List<string> {".png", "jpg", ".jpeg", ".gif", ".bmp"};
 
         #endregion
 
@@ -26,7 +25,7 @@ namespace App.Web.Controllers
         public ActionResult Index()
         {
             var generalSettings = _db.GeneralSettings.ToDictionary(x => x.SettingName);
-            ViewData["Address"] = generalSettings["Address"].SettingName ?? string.Empty;
+            ViewData["Address"] = generalSettings["Address"].SettingValue ?? string.Empty;
             //ViewData["SiteLogo"] = generalSettings[1].ToString();
             ViewData["SiteName"] = generalSettings["SiteName"].SettingValue ?? string.Empty;
             ViewData["SmsPassword"] = generalSettings["SmsPassword"].SettingValue ?? string.Empty;
@@ -34,7 +33,7 @@ namespace App.Web.Controllers
             ViewData["SmsUrl"] = generalSettings["SmsUrl"].SettingValue ?? string.Empty;
             ViewData["SmsUser"] = generalSettings["SmsUser"].SettingValue ?? string.Empty;
 
-            ViewData["IsLogoExist"] = _db.GeneralSettings.Count(x=>x.SettingName == "SiteLogo")>0;
+            ViewData["IsLogoExist"] = _db.GeneralSettings.Count(x => x.SettingName == "SiteLogo") > 0;
             return View();
         }
 
@@ -43,23 +42,37 @@ namespace App.Web.Controllers
         {
             var generalSettings = new List<GeneralSetting>();
             var siteLogo = Request.Files["SiteLogo"];
-            
-            if (siteLogo.ContentLength > 0 )
+
+            // ReSharper disable once PossibleNullReferenceException
+            if (siteLogo.ContentLength > 0)
             {
                 // 1048567 bytes = 1 MegaBytes
-                if (siteLogo.FileName == string.Empty || siteLogo.ContentLength > 1048576) return View();
+                if (siteLogo.FileName == string.Empty || siteLogo.ContentLength > 1048576)
+                {
+                    TempData["Toastr"] = @"toastr.error('Max file size: 1 MB!', 'Error!');";
+                    return RedirectToAction("Index");
+                }
                 var extension = Path.GetExtension(siteLogo.FileName);
-                
+
                 if (extension == null) return View();
                 extension = extension.ToLower();
-                if (_allowedLogoFileTypes.IndexOf(extension) == -1) return View();
+                if (_allowedLogoFileTypes.IndexOf(extension) == -1)
+                {
+                    TempData["Toastr"] =
+                        @"toastr.error('Only .png, .jpg, .jpeg, .gif, .bmp file types allowed.', 'Error!');";
+                    return RedirectToAction("Index");
+                }
 
                 var image = Image.FromStream(siteLogo.InputStream);
-                if (image.Width != 256 || image.Height != 256) return View();
+                if (image.Width != 256 || image.Height != 256)
+                {
+                    TempData["Toastr"] = @"toastr.error('Image size should be 256 Pixel x 256 Pixel', 'Error!');";
+                    return RedirectToAction("Index");
+                }
 
                 var setting = new GeneralSetting {SettingName = "SiteLogo", SettingValue = siteLogo.FileName};
                 generalSettings.Add(setting);
-                siteLogo.SaveAs(Server.MapPath("~/Content/Template/img/site/site-logo"+extension));
+                siteLogo.SaveAs(Server.MapPath("~/Content/Template/img/site/site-logo" + extension));
             }
 
             collection.AllKeys.ForEach(key =>
@@ -78,7 +91,7 @@ namespace App.Web.Controllers
                         {
                             _db.GeneralSettings
                                 .Where(x => x.SettingName == setting.SettingName)
-                                .Update(u => new GeneralSetting { SettingValue = setting.SettingValue });
+                                .Update(u => new GeneralSetting {SettingValue = setting.SettingValue});
 
                             _db.SaveChanges();
                         }
@@ -91,19 +104,43 @@ namespace App.Web.Controllers
 
                     dbTransaction.Commit();
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    TempData["Toastr"] = String.Format("toastr.error('{0}', 'DB Error!');", ex.Message);
                     dbTransaction.Rollback();
                 }
             }
-            
+            TempData["Toastr"] = @"toastr.success('General Setting Saved!', 'Success!');";
             return RedirectToAction("Index", "GeneralSettings");
         }
 
         public JsonResult IsLogoHaveToAdd()
         {
-            var flag =  _db.GeneralSettings.Count(x => x.SettingName == "SiteLogo") <= 0;
+            var flag = _db.GeneralSettings.Count(x => x.SettingName == "SiteLogo") <= 0;
             return Json(flag, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CreateDbBackUp()
+        {
+            try
+            {
+                var backUpDirectory = Server.MapPath("~/Backup/");
+                var fileName = "CRM_"+DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".bak";
+                var backupPath = Path.Combine(backUpDirectory, fileName);
+                if (!Directory.Exists(backUpDirectory)) Directory.CreateDirectory(backUpDirectory);
+
+                var query = string.Format("BACKUP DATABASE [CRM_DB] TO DISK ='{0}'", backupPath);
+                _db.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, query);
+
+                var fileBytes = System.IO.File.ReadAllBytes(backupPath);
+                return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            }
+            catch (Exception)
+            {
+                TempData["Toastr"] = @"toastr.error('Database backup cannot be created!', 'DB Error!');";
+                return RedirectToAction("Index");
+            }
+            
         }
     }
 }
