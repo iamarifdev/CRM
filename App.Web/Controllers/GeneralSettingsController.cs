37b474a466c29d5e1fccd01c1218d39ba6b9d162
Md. Ariful Islam <arifjmamun24@gmail.com>
2017-07-28 11:54:34 +0600

    General Setting Layout Designed

---
+++ App.Web/Controllers/GeneralSettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{
+    public class GeneralSettingsController : Controller
+    {
+        // GET: GeneralSettings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(FormCollection collection)
+        {
+
+            return RedirectToAction("Index", "GeneralSettings");
+        }
+    }
+}
