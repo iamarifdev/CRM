8ceb662813a854cd623d298f9ef898da83370c06
Md. Ariful Islam <arifjmamun24@gmail.com>
2017-11-08 00:04:00 +0600

    Added a perfect Pure Javascript method for Printing document on demand by divId

--- App.Web/Scripts/print.js
+++ App.Web/Scripts/print.js
@@ -1,8 +1,28 @@
-﻿function printDiv(divName) {
+﻿// This one freezes the content
+function printDiv(divName) {
     var printContents = document.getElementById(divName).innerHTML;
     var originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
     document.location.reload(true);
+}
+
+// This one perfectly working [100% working]
+// Pure Javascript
+function PrintDocumentDivById(divId) {
+    //Works with Chome, Firefox, IE, Safari
+    var headElement = document.getElementsByTagName("head")[0].innerHTML;
+    var divElements = document.getElementById(divId).innerHTML;
+    var printWindow = window.open("", "_blank", "");
+    //open the window
+    printWindow.document.open();
+    printWindow.document.write("<html>" + headElement + "<body>" + divElements + "</body></html>");
+    printWindow.document.close();
+    printWindow.focus();
+    //The Timeout is ONLY to make Safari work, but it still works with FF, IE & Chrome.
+    setTimeout(function () {
+        printWindow.print();
+        printWindow.close();
+    }, 100);
 }
