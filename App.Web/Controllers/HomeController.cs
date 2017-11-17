0749963bd28a330400f3916cce90effe28d76c68
Md. Ariful Islam <arifjmamun24@gmail.com>
2017-11-18 01:29:08 +0600

    CRM Dashboard Managed

--- App.Web/Controllers/HomeController.cs
+++ App.Web/Controllers/HomeController.cs
@@ -1,13 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using App.Entity.Models;
+using App.Web.Context;
+using App.Web.Helper;
 
 namespace App.Web.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        #region Private Zone
+        private readonly CrmDbContext _db;
+        #endregion
+        public HomeController()
+        {
+            _db = new CrmDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["Toastr"] = Toastr.DbError(ex.Message);
+                return RedirectToAction("Index", "Home");
+            }
+            
         }
 
         [ChildActionOnly]
